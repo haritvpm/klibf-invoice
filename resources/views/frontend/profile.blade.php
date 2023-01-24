1ba4ee47f9c74fea2d13a9bc1a74f9cad46fd848
hari <harirs@gmail.com>
2023-01-24 09:30:20 +0530

    profile edit team details

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     {{ trans('global.my_profile') }}
@@ -49,26 +49,28 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
+        </div>
+        <div class="row">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     {{ trans('global.change_password') }}
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route("frontend.profile.password") }}">
+                    <form class="form-inline" method="POST" action="{{ route("frontend.profile.password") }}">
                         @csrf
-                        <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+                        <div class="ml-2 form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                             <label class="required" for="password">New {{ trans('cruds.user.fields.password') }}</label>
-                            <input class="form-control" type="password" name="password" id="password" required>
+                            <input class="ml-2 form-control" type="password" name="password" id="password" required>
                             @if($errors->has('password'))
                                 <span class="help-block" role="alert">{{ $errors->first('password') }}</span>
                             @endif
                         </div>
-                        <div class="form-group">
+                        <div class="ml-2 form-group">
                             <label class="required" for="password_confirmation">Repeat New {{ trans('cruds.user.fields.password') }}</label>
-                            <input class="form-control" type="password" name="password_confirmation" id="password_confirmation" required>
+                            <input class="ml-2 form-control" type="password" name="password_confirmation" id="password_confirmation" required>
                         </div>
-                        <div class="form-group">
+                        <div class="ml-2 form-group">
                             <button class="btn btn-danger" type="submit">
                                 {{ trans('global.save') }}
                             </button>
